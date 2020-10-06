73d0d94bf8f752461ac8f6c3525b065a0150e0bd
ahonn <ahonn95@outlook.com>
2020-10-06 16:49:50 +0800

    feat: add dida365 task via puppeteer

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,41 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import puppeteer from 'puppeteer'
+
+function validateInput(name: string): void {
+  if (!core.getInput(name)) throw new Error(`${name} is a required input`)
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    validateInput('emailOrPhone')
+    validateInput('password')
+    validateInput('task')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://dida365.com/signin')
+
+    await page.focus('#emailOrPhone')
+    await page.keyboard.type(core.getInput('emailOrPhone'))
+
+    await page.focus('#password')
+    await page.keyboard.type(core.getInput('password'))
+
+    await Promise.all([
+      page.click('#app>div>div>div>section:nth-child(1)>button'),
+      page.waitForNavigation({waitUntil: 'networkidle0'})
+    ])
+
+    await page.click('#add-task')
+    await page.keyboard.type(core.getInput('task'))
+    await Promise.all([
+      page.keyboard.press('Enter'),
+      page.waitForNavigation({waitUntil: 'networkidle0'})
+    ])
+
+    // await page.screenshot({path: 'example.png'})
+
+    await browser.close()
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
